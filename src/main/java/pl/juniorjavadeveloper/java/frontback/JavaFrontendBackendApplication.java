d9ea865ee0dd6c63cd73a41b08faf184f25018ba
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-03-22 10:37:49 +0100

    Add Spring Boot template project generated from https://start.spring.io together with Maven Wrapper

---
+++ src/main/java/pl/juniorjavadeveloper/java/frontback/JavaFrontendBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.juniorjavadeveloper.java.frontback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFrontendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFrontendBackendApplication.class, args);
+	}
+
+}
