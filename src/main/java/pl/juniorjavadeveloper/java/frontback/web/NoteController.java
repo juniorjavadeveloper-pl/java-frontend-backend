7ae65b156d12fda502281190c9248a8d42990a34
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-03-25 07:27:13 +0100

    Add simple html page and Controller methods for add Note functionality

--- src/main/java/pl/juniorjavadeveloper/java/frontback/web/NoteController.java
+++ src/main/java/pl/juniorjavadeveloper/java/frontback/web/NoteController.java
@@ -1,6 +1,8 @@
 package pl.juniorjavadeveloper.java.frontback.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -13,11 +15,18 @@ public class NoteController {
     public static final Logger LOGGER = Logger.getLogger(NoteController.class.getName());
     private List<NoteModel> notes = new ArrayList<>();
 
+    @GetMapping
     public String list() {
         LOGGER.info("list() = " + notes);
         return "notes/notes";
     }
 
+    @GetMapping(value = "/create")
+    public String createView() {
+        return "notes/add-note";
+    }
+
+    @PostMapping
     public String create(NoteModel noteModel) {
         LOGGER.info("create(" + noteModel + ")");
         notes.add(noteModel);
